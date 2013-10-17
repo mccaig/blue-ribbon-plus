2037669a8535e1f1c884b1849749f0812a4ddadb
Rhys Anthony McCaig <mccaig@gmail.com>
2013-10-16 23:16:02 -0400

    deleted:    setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='blue-ribbon-plus',
-	version='1.0',
-	description='Library for retrieving data from Nike+',
-	author='Rhys Anthony McCaig',
-	author_email='mccaig@gmail.com',
-	url='https://github.com/rhysmccaig/blue-ribbon-plus',
-	py_modules=['nikeplus'],
-	)
